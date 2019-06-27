c1ebd28d9819924d6d7785e7affc0e141b1695a8
Filippo Simini <filippo.simini@gmail.com>
2019-06-27 15:46:48 -0500

    added option to filter and compress geolife trajectories

--- skmob/io/file.py
+++ skmob/io/file.py
@@ -4,6 +4,7 @@ import os
 import fnmatch
 from ..core.trajectorydataframe import TrajDataFrame
 from ..core.flowdataframe import FlowDataFrame
+from ..preprocessing import filtering, compression
 
 
 def write(skmob_df, file):
@@ -32,7 +33,9 @@ def read(file):
         return None
 
 
-def load_geolife_trajectories(path_to_geolife_data_dir, user_ids=[]):
+def load_geolife_trajectories(path_to_geolife_data_dir, user_ids=[],
+                              filter_kwargs={'max_speed_kmh': 400},
+                              compress_kwargs={'spatial_radius_km': 0.2}):
     """
     Load the Geolife trajectories in a skmob.TrajDataFrame
 
@@ -41,6 +44,12 @@ def load_geolife_trajectories(path_to_geolife_data_dir, user_ids=[]):
 
     :param user_ids: list
         list of user IDs to load. If empty all users are loaded.
+
+    :param filter_kwargs: dict
+        arguments of `preprocessing.filtering.filter()`. If empty, data is not filtered.
+
+    :param compress_kwargs: dict
+        arguments of `preprocessing.compression.compress()`. If empty, data is not compressed.
 
     :return: TrajDataFrame
         a TrajDataFrame containing all trajectories
@@ -53,16 +62,28 @@ def load_geolife_trajectories(path_to_geolife_data_dir, user_ids=[]):
         user_ids = os.listdir(path)
 
     for uid in user_ids:
+
         try:
             all_files = fnmatch.filter(os.listdir(path + '%s/Trajectory/' % uid), "*.plt")
+
         except NotADirectoryError:
             continue
+
         dfg = (pd.read_csv(path + '%s/Trajectory/' % uid + f,
                            skiprows=6, header=None, usecols=[0, 1, 5, 6]) for f in all_files)
         df = pd.concat(dfg, ignore_index=True)
         df['datetime'] = df[5] + ' ' + df[6]
         df.drop(columns=[5, 6], inplace=True)
         df['uid'] = [str(uid) for i in range(len(df))]
-        tdf = tdf.append(TrajDataFrame(df, latitude=0, longitude=1))
+
+        tdf0 = TrajDataFrame(df, latitude=0, longitude=1)
+
+        if len(filter_kwargs) > 0:
+            tdf0 = filtering.filter(tdf0, **filter_kwargs)
+
+        if len(compress_kwargs) > 0:
+            tdf0 = compression.compress(tdf0, **compress_kwargs)
+
+        tdf = tdf.append(tdf0)
 
     return tdf
