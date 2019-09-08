haversine distance calculation incorrectly used lat for lon, lon for lat

skmob/preprocessing/clustering.py
@@ -91,7 +91,7 @@ def group_by_label(X, labels):
 
 def _cluster_array(lat_lng_dtime_other, cluster_radius_km, min_samples, verbose=False):
 
-    X = np.array([[point[1], point[0]] for point in lat_lng_dtime_other])
+    X = np.array([[point[0], point[1]] for point in lat_lng_dtime_other])
 
     # Compute DBSCAN
     eps_rad = cluster_radius_km / kms_per_radian