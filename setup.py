e2f926aa8a49fc1084a0784bbcb75b2fc6da2d51
Roberto Pellungrini <roberto.pellungrini@di.unipi.it>
2019-12-04 19:31:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+from setuptools import setup
+import sys
+if sys.version_info >= (3,8):
+    sys.exit('Sorry, Python > 3.7 is not supported')
 from setuptools import setup, find_packages
 
 long_description = open('README.md').read()
