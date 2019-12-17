ba79e2256a7164df4eaad132fc6e19acfe2cb200
lucpappalard <lucapappalardo1984@gmail.com>
2019-12-17 18:49:16 +0100

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -73,3 +73,6 @@ numpydoc_show_class_members = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_javascript('copybutton.js')
