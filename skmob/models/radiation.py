73ef184d5d4856a614b14f5b6d7f79d9c18623b2
lucpappalard <lucapappalardo1984@gmail.com>
2020-08-06 11:59:04 +0200

    updated urls examples doc

--- skmob/models/radiation.py
+++ skmob/models/radiation.py
@@ -40,7 +40,7 @@ class Radiation:
     >>> import numpy as np
     >>> from skmob.models import Radiation
     >>> # load a spatial tessellation
-    >>> url_tess = 'https://raw.githubusercontent.com/scikit-mobility/scikit-mobility/master/tutorial/data/NY_counties_2011.geojson'
+    >>> url_tess = 'https://github.com/scikit-mobility/tutorials/blob/master/mda_masterbd2020/data/NY_counties_2011.geojson'
     >>> tessellation = gpd.read_file(url_tess).rename(columns={'tile_id': 'tile_ID'})
     >>> print(tessellation.head())
       tile_ID  population                                           geometry
@@ -50,7 +50,7 @@ class Radiation:
     3   36059     1346176  POLYGON ((-73.707662 40.727831, -73.700272 40....
     4   36011       79693  POLYGON ((-76.279067 42.785866, -76.2753479999...    
     >>> # load real flows into a FlowDataFrame
-    >>> # download the file with the real fluxes from: https://raw.githubusercontent.com/scikit-mobility/scikit-mobility/master/tutorial/data/NY_commuting_flows_2011.csv
+    >>> # download the file with the real fluxes from: https://github.com/scikit-mobility/tutorials/blob/master/mda_masterbd2020/data/NY_commuting_flows_2011.csv
     >>> fdf = skmob.FlowDataFrame.from_file("NY_commuting_flows_2011.csv", 
                                             tessellation=tessellation, 
                                             tile_id='tile_ID', 
