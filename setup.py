58c431d107a3249e8fbab6edb7fae8cf19427d18
gbarlacchi <pestello87*>
2021-03-06 10:17:37 +0100

    added corect version in setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ EXTRAS_REQUIRE = {'test': TESTS_REQUIRES}
 setup(
     name='scikit-mobility',
     packages=find_packages(include=["skmob", "skmob.*"]),
-    version='1.2.1',
+    version='1.2.2',
     extras_require=EXTRAS_REQUIRE,
     license='new BSD',
     python_requires='>=3.6',
