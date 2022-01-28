f0c7435276dfff3d79f8fb6a6842bb77cfbd80a5
Michele Ferretti <miccferr@users.noreply.github.com>
2022-01-28 10:08:29 +0100

    Release lib version 1.2.3 (#211)
    * bump version
    
    * update to flit
    
    * delete lock
    
    * update dependencies
    
    * fix TOML
    
    * fix TOML
    
    * Add license
    
    * add license
    
    * add maintainers
    
    * add metadata
    
    * update metadata
    
    * update license
    
    * fix version typos
    
    * update python requirements
    
    * update metadata
    
    * update requirements
    
    * fix pandas version
    
    * add dunder var
    
    * remove version from toml
    
    * use poetry include
    
    * add license classifier
    
    * update python requirements
    
    * add test for module versions
    
    * format whitespace
    
    * fix python dependency
    
    * add poetry lock
    
    * explicit naming of test file
    
    * remove lower boundary in dependencies
    
    * update version
    
    * add pre-commit
    
    * relax package version constraints
    
    * update lock
    
    * bump version
    
    * relax pandas requirement
    
    * update lock
    
    * add explicit numpy dependency
    
    * add explicit python version for numpy dependency
    
    * fix dependencies
    
    * fix imports
    
    * update lock
    
    * bump version
    
    * update lock

--- skmob/__init__.py
+++ skmob/__init__.py
@@ -1,2 +1,7 @@
 "A toolbox for analyzing and processing mobility data."
-__version__ = "1.2.3-alpha.2"
+
+__version__ = "1.2.3"
+
+from .core.flowdataframe import FlowDataFrame  # noqa
+from .core.trajectorydataframe import TrajDataFrame  # noqa
+from .io.file import read, write  # noqa
