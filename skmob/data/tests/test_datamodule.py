f1bca652433d90bb91131b69ba2675f021f3ac27
Giuliano Cornacchia <37023673+GiulianoCornacchia@users.noreply.github.com>
2022-05-06 14:40:15 +0200

    data module

--- skmob/data/tests/test_datamodule.py
+++ skmob/data/tests/test_datamodule.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from skmob.data.load import load_dataset, list_datasets
 
 # fix a random seed
 np.random.seed(2)
@@ -8,6 +9,7 @@ np.random.seed(2)
 @pytest.mark.parametrize('dataset_names', ['flow_foursquare_nyc', 'foursquare_nyc', 
                                            'nyc_boundaries', 'parking_san_francisco', 
                                            'taxi_san_francisco'])
+
 @pytest.mark.parametrize('dataset_types', ['trajectory', 'flow', 'shape', 'auxiliar'])
 
 def test_foo(dataset_names, dataset_types):
