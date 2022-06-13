824e906b123c3696ea80d6b9dd73f0564b4c1e6b
Michele Ferretti <miccferr@users.noreply.github.com>
2022-06-13 18:12:23 +0200

    Update __init__.py

--- skmob/__init__.py
+++ skmob/__init__.py
@@ -1,6 +1,6 @@
 "A toolbox for analyzing and processing mobility data."
 
-__version__ = "1.3.0-alpha.5"
+__version__ = "1.3.0"
 
 from .core.flowdataframe import FlowDataFrame  # noqa
 from .core.trajectorydataframe import TrajDataFrame  # noqa
