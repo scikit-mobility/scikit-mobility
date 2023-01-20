e941fb3e5ca9757d3fbdfd5cd0bf17caf635ac0d
gbarlacchi <pestello87*>
2023-01-20 18:40:05 +0100

    Fix CI doc builder

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,7 @@ numpydoc_show_class_members = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["./static/html/_static"]
+html_static_path = ["_static"]
 
 
 def setup(app):
